94f64ca1862bfeab894895ad516c18adb649522a
GabrielRioo <gabrielpaivario@gmail.com>
2022-06-01 18:24:06 -0300

    adicionando imagem para cada pokemon

--- pokedex/src/components/BoxPokemon.tsx
+++ pokedex/src/components/BoxPokemon.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from 'react';
 import '../assets/styles/BoxPokemon.css'
+import { useFetch } from '../hooks/useFetch';
 
-export function BoxPokemon({pokemonName}: any) {
+type Pokemon = {
+  id: number;
+  sprites: {
+    other: {
+      home: {
+        front_default: string;
+      };
+    };
+  };
+  types: [
+    {
+      slot: number;
+      type: {
+        name: string;
+        url: string;
+      };
+    }
+  ];
+};
+
+export function BoxPokemon({pokemonName, pokemonId}: any) {
+  const [pokemonSprite, setPokemonSprite] = useState<string | undefined>("");
+
+    const { data: pokemon } = useFetch<Pokemon>(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+    );
+
+    useEffect(() => {
+      setPokemonSprite(pokemon?.sprites?.other?.home.front_default)
+    }, [])
+
   return(
       <div className="box-pokemon">
+        {/* {pokemon?.id == pokemonId &&} */}
+        <img src={pokemonSprite} alt="Imagem do Pokemon" className="img-pokemon"/>
         <p>{pokemonName}</p>
       </div>
   )
--- pokedex/src/pages/search_pokemon/SearchPokemon.tsx
+++ pokedex/src/pages/search_pokemon/SearchPokemon.tsx
@@ -1,21 +1,50 @@
+import { useEffect, useState } from "react";
 import { BoxPokemon } from "../../components/BoxPokemon";
 import { useFetch } from "../../hooks/useFetch";
 
-type Pokemon = {
+type AllPokemon = {
   count: number;
   next: string;
   previous?: any;
   results: [{ name: string; url: string }];
 };
 
+
+
+type PokemonType = {
+  results: [
+    {
+      name: string;
+    }
+  ];
+};
+
 export function SearchPokemon() {
-  const { data } = useFetch<Pokemon>("https://pokeapi.co/api/v2/pokemon");
+
+  const { data: all_pokemons } = useFetch<AllPokemon>(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+
+  // const { data: pokemon } = useFetch<Pokemon>(
+  //   `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+  // );
 
   return (
     <>
-      <div className="box-pokemon-content" >
-        {data?.results?.map((item) => {
-          return <BoxPokemon pokemonName={item.name} key={item.name}/>;
+      <div className="box-pokemon-content">
+        {all_pokemons?.results?.map((item) => {
+          // setPokemonId(item.url.split('/').at(-2)?.toString());
+          // {PokemonInfo(item.url.split('/').at(-2)?.toString())}
+
+          return (
+            <>
+              <BoxPokemon
+                pokemonName={item.name}
+                pokemonId={item.url.split("/").at(-2)?.toString()}
+                key={item.name}
+              />
+            </>
+          );
         })}
       </div>
     </>
